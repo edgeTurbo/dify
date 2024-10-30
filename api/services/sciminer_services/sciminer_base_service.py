67106131d5fc8f5dd95594df7d07b6fd1df54de7
Edge Turbo <niuzhenjiang@protonunfold.com>
2024-10-30 15:59:02 +0800

    1. 提取task_label任务标签，用于前端历史记录展示用的，放置在SciminerBaseService基类上

--- api/services/sciminer_services/sciminer_base_service.py
+++ api/services/sciminer_services/sciminer_base_service.py
@@ -11,6 +11,7 @@ from models.model import EndUser
 
 class SciminerBaseService:
     service_type = None
+    task_label = None
 
     def get_service_result_data(self, task_id: str, user: Union[Account, EndUser]):
         """
