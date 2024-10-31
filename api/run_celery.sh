b6e57aa50e18308c0033f640f1c78116abc15144
Edge Turbo <niuzhenjiang@protonunfold.com>
2024-10-31 16:33:13 +0800

    1. 新增启动celery项目的脚本

--- api/run_celery.sh
+++ api/run_celery.sh
@@ -13,7 +13,7 @@ done
 
 # 在调试模式下输出详细信息
 if [ "$DEBUG" == true ]; then
-  echo "Running in debug mode...log file will be saved to app.log"
+  echo "Running in debug mode...log file will be saved to celery.log"
   nohup celery -A app.celery worker -P gevent -c 1 --loglevel DEBUG -Q molecular_docking,global_docking > celery.log 2>&1 &
 else
   echo "Running in production mode...will not log to file"
